c1f854dd4a2982fa2691abe404f7f8aefefdf57a
Ram Uday Kumar <56128152+ThankKumar@users.noreply.github.com>
2022-07-22 07:31:09 +0530

    X and H pattern using Println()

---
+++ 21-jul-2022/HomeWork/Hpattern.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package homework;
+
+public class Hpattern {
+
+    public static void main(String[] args) {
+        System.out.println("Print H pattern using *");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println(" ************* ");
+        System.out.println(" ************* ");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("**           **");
+        System.out.println("**           **");
+
+    }
+}
